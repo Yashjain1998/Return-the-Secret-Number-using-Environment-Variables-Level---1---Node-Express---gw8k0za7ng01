f3a610f1d244fdfb43856a6d42ae9d8f35c1e46e
yash8mol <jainy2177@gmail.com>
2023-07-23 22:12:19 +0530

    code

--- src/app.js
+++ src/app.js
@@ -10,6 +10,17 @@ app.use(express.json());
 */
 app.get('/api/get-env', (req, res) => {
    //Write your code here
+   try {
+    const number=process.env.NUMBER;
+    if(number){
+        res.status(200).json({"number":number});
+    }else{
+        res.status(404).json({ "err": "Not Found"});
+    } 
+   } catch (error) {
+    res.status(404).json({ "err": "Not Found"});
+   }
+  
 });
 
 module.exports = app;
